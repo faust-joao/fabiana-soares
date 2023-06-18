393dcf39932312446befab7b61453e1e58cbb052
faust-joao <joao.vsf2004@aluno.ifsc.edu.br>
2023-06-18 16:19:38 -0300

    Adiciona carrosel de imagens

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+const content = document.querySelectorAll('.content > *');
+let currentIndex = 0;
+
+document.querySelector('#next').addEventListener('click', () => {
+    currentIndex++;
+
+    if (currentIndex === content.length) {
+        currentIndex = 0;
+        content[content.length - 1].style.display = 'none';
+    } else {
+        content[currentIndex - 1].style.display = 'none';
+    }
+
+    content[currentIndex].style.display = 'block';
+});
+
+document.querySelector('#previous').addEventListener('click', () => {
+    if (currentIndex === 0) {
+        content[currentIndex].style.display = 'none';
+        content[content.length - 1].style.display = 'block';
+        currentIndex = content.length - 1;
+        return;
+    }
+    
+    content[currentIndex].style.display = 'none';
+    currentIndex--;
+    content[currentIndex].style.display = 'block';
+});
